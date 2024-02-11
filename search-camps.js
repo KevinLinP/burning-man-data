e15fb0ad4ce159a6d1ca86ecdb4399de75fef207
Kevin Lin <kevin.lin.p@gmail.com>
2024-02-10 23:14:47 -0800

    add search camps

---
+++ search-camps.js
@@ -0,0 +1,47 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { getEmbedding } from "./lib/openai.js";
+import { loadCamps } from "./lib/load-data.js";
+
+const searchCamps = async ({ query }) => {
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("burning-man-data");
+
+  const camps = loadCamps();
+
+  const queryEmbedding = await getEmbedding({ text: query });
+  const queryResponse = await index.query({
+    vector: queryEmbedding,
+    topK: 10,
+  });
+  console.log("usage", queryResponse.usage);
+  console.log("");
+
+  const campUidsAndScores = queryResponse.matches.reduce((camps, match) => {
+    const campId = match.id.split("|")[1];
+    if (camps[campId]) {
+      return camps;
+    } else {
+      const score = match.score;
+      return { ...camps, [campId]: score };
+    }
+  }, {});
+
+  const campStrings = Object.entries(campUidsAndScores).map(
+    ([campId, score], i) => {
+      const camp = camps[campId];
+      let string = `${i + 1}. ${camp.name} (${score})`;
+      if (camp.description) {
+        string += `\n${camp.description}`;
+      }
+      return string;
+    }
+  );
+
+  console.log(campStrings.join("\n\n"));
+  console.log("");
+};
+
+const query = process.argv.slice(2).join(" ");
+searchCamps({ query });
